56e1c5f0d7d639c3108f6938ff7dfecb28e138b9
HamidrezaRzp <hamidreza.bigred3@gmail.com>
2025-08-28 18:00:05 +0330

    fix: product models bug fixed

--- ecommerce/products/models.py
+++ ecommerce/products/models.py
@@ -26,7 +26,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=5, decimal_places=2)
     stock = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateField(auto_now=False)
+    updated_at = models.DateField(auto_now=True)
 
     class Meta:
         verbose_name = _('product')
