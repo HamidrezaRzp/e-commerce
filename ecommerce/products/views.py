1d50cbb1c738d7f614d4a2a14ca9e8257d3bfd18
HamidrezaRzp <hamidreza.bigred3@gmail.com>
2025-08-30 15:53:58 +0330

    feat: viewset for product model add

--- ecommerce/products/views.py
+++ ecommerce/products/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from .models import Product, Category
+from .serializers import ProductSerializer
+from rest_framework import viewsets
+from .permissions import ProductPermissions
+from rest_framework.response import Response
 
-# Create your views here.
+class ProductViewset(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = [ProductPermissions]
+
+    def perform_create(self, serializer):
+        serializer.save(seller=self.request.user)
