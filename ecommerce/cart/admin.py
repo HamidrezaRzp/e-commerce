3b600f9409c4bb3441753e112dfb4cc85c6c6145
HamidrezaRzp <hamidreza.bigred3@gmail.com>
2025-09-21 02:07:38 +0330

    feat: add cart & cartitem model&admin &  migrate to DB

--- ecommerce/cart/admin.py
+++ ecommerce/cart/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Cart, CartItme
 
-# Register your models here.
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ['user', 'created_at']
+    list_filter = ['user', 'created_at']
+    search_fields = ['user', 'created_at']
+    ordering = ('created_at','updated_at',)
+
+
+@admin.register(CartItme)
+class CartItmeAdmin(admin.ModelAdmin):
+    list_display = ['cart', 'product','quantity']
+    list_filter = ['cart']
+    search_fields = ['cart']
+    ordering = ('cart',)
--- ecommerce/cart/models.py
+++ ecommerce/cart/models.py
@@ -1,2 +1,39 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from products.models import Product
+from django.db.models import UniqueConstraint
 
+
+User = get_user_model()
+
+
+class Cart(models.Model):
+    user = models.ForeignKey(User,
+                                on_delete=models.CASCADE,
+                                related_name='carts')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = ('cart')
+        verbose_name_plural = ('carts')
+
+    def __str__(self):
+        return f"Cart {self.id} for {self.user.email}"
+
+
+class CartItme(models.Model):
+    cart = models.ForeignKey(Cart,
+                                on_delete=models.CASCADE,
+                                related_name='cartitems')
+    product = models.ForeignKey(Product,
+                                   on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    class Meta:
+        constraints = [
+            UniqueConstraint(fields=["cart", "product"], name="unique_cart_item_per_prodct")
+        ]
+
+    def __str__(self):
+        return f"{self.quantity}x {self.product.name} in cart:{self.cart.id}"
